a721ef3f254065cfb7e7ffa00ef5797324242f38
GianBaeza <gianb04@gmail.com>
2024-06-11 18:21:41 -0300

    agregamos display grid en las card en vez de display flex

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 const nav = document.querySelector("#nav");
 
-abrir.addEventListener("click", () => {
-  nav.classList.add("nav-visible");
-});
-cerrar.addEventListener("click", () => {
-  nav.classList.remove("nav-visible");
-  
+document.addEventListener("DOMContentLoaded", () => {
+  abrir.addEventListener("click", () => {
+    nav.classList.add("nav-visible");
+  });
+  cerrar.addEventListener("click", () => {
+    nav.classList.remove("nav-visible");
+  });
 });
