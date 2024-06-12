4af9791cfc82c14df4cc4e8f4779dbe2caca0757
GianBaeza <gianb04@gmail.com>
2024-06-12 15:50:29 -0300

    pude mostrar las imagenes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 const nav = document.querySelector("#nav");
 const carrusel = document.querySelector(".carrusel-items");
+const containerCards = document.getElementById("container-Card");
 
 document.addEventListener("DOMContentLoaded", () => {
   abrir.addEventListener("click", () => {
@@ -18,9 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
     intervalo = setInterval(function () {
       carrusel.scrollLeft = carrusel.scrollLeft + step;
       if (carrusel.scrollLeft == maxScrollLeft) {
-        step = step * 1;
-      } else if (carrusel.scrollLeft <= 0) {
         step = step * -1;
+      } else if (carrusel.scrollLeft <= 0) {
+        step = step * 1;
       }
     }, 10);
   };
@@ -36,8 +37,6 @@ document.addEventListener("DOMContentLoaded", () => {
     start();
   });
 
-  let productos = [];
-
   const url = "productos.json";
   function cargarProductos() {
     fetch(url)
@@ -48,7 +47,9 @@ document.addEventListener("DOMContentLoaded", () => {
         return response.json();
       })
       .then((data) => {
-        productos = data.products;
+        const productos = data.map((item) => {
+          return ({ id, titulo, imagen, categoria, precio } = item);
+        });
         mostrarProductosCard(productos);
       })
       .catch((err) => {
@@ -57,61 +58,66 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   cargarProductos();
 
-  //las creao para depsues mostrarlas
-
   function mostrarProductosCard(productos) {
-    const containerCards = document.getElementById("container-Card");
-    productos.forEach((item) => {
+    containerCards.innerHTML = "";
+    console.log(productos);
+    productos.forEach((item, index) => {
+      console.log(index)
+      const { id, imagen, titulo, precio, sizes, categoria } = item;
       const divCard = document.createElement("div");
       divCard.classList.add("card");
 
       const carrusel = document.createElement("div");
-      carrusel.id = "carouselExample";
+      carrusel.id = `carouselExample${index}`;
       carrusel.classList.add("carousel", "slide");
 
-      const contenedorIntertoCarrusel = document.createElement("div");
-      contenedorIntertoCarrusel.classList.add("carousel-inner");
-      console.log(item.image)
-      item.image.forEach((image, index) => {
+      const carouselInner = document.createElement("div");
+      carouselInner.classList.add("carousel-inner");
+
+      imagen.forEach((imgSrc, imageIndex) => {
         const carouselItem = document.createElement("div");
         carouselItem.classList.add("carousel-item");
-        if (index === 0) {
-          carouselItem.classList.add("activo");
+
+        if (imageIndex === 0) {
+          carouselItem.classList.add("active");
         }
+
         const img = document.createElement("img");
-        img.src = image;
+        img.src = imgSrc;
         img.alt = "img";
-        img.classList.add("card-img");
+        img.classList.add("d-block", "w-100", "card-img");
         carouselItem.appendChild(img);
-        contenedorIntertoCarrusel.appendChild(carouselItem);
+
+        carouselInner.appendChild(carouselItem);
       });
-      carrusel.appendChild(contenedorIntertoCarrusel);
+
+      carrusel.appendChild(carouselInner);
 
       const buttonAntes = document.createElement("button");
       buttonAntes.classList.add("carousel-control-prev");
       buttonAntes.type = "button";
-      buttonAntes.setAttribute("data-bs-target", "#carouselExample");
+      buttonAntes.setAttribute("data-bs-target", `#carouselExample${index}`);
       buttonAntes.setAttribute("data-bs-slide", "prev");
       buttonAntes.innerHTML = `<span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span>`;
 
       const buttonSigiente = document.createElement("button");
       buttonSigiente.classList.add("carousel-control-next");
       buttonSigiente.type = "button";
-      buttonSigiente.setAttribute("data-bs-target", "#carouselExample");
+      buttonSigiente.setAttribute("data-bs-target", `#carouselExample${index}`);
       buttonSigiente.setAttribute("data-bs-slide", "next");
-
       buttonSigiente.innerHTML = `<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span>`;
+
       carrusel.appendChild(buttonAntes);
       carrusel.appendChild(buttonSigiente);
       divCard.appendChild(carrusel);
 
       const titleCard = document.createElement("h3");
       titleCard.classList.add("card-title");
-      titleCard.textContent = item.name;
+      titleCard.textContent = titulo;
 
-      const price = document.createElement("p");
-      price.classList.add("card-precio");
-      price.textContent = item.price;
+      const priceElement = document.createElement("p");
+      priceElement.classList.add("card-precio");
+      priceElement.textContent = `$ ${precio}`
 
       const containerButtonCar = document.createElement("div");
       containerButtonCar.classList.add("container-button-card");
@@ -119,12 +125,18 @@ document.addEventListener("DOMContentLoaded", () => {
       const buttonComprar = document.createElement("button");
       buttonComprar.classList.add("button-card");
       buttonComprar.textContent = "Comprar";
-      buttonComprar.addEventListener("click", comprarItem()); //CREAR LA FUINCION COMPRA RITEM
+      // buttonComprar.addEventListener("click", comprarItem()); //CREAR LA FUNCION COMPRA RITEM
 
       const buttonVer = document.createElement("button");
       buttonVer.classList.add("button-card");
       buttonVer.textContent = "Ver";
-      buttonVer.addEventListener("click", openModal()); // CRERAR LA FUNCION OPEN MODAL
+      // buttonVer.addEventListener("click", openModal()); // CRERAR LA FUNCION OPEN MODAL
+
+      divCard.appendChild(titleCard);
+      divCard.appendChild(priceElement);
+      containerButtonCar.appendChild(buttonComprar);
+      containerButtonCar.appendChild(buttonVer);
+      divCard.appendChild(containerButtonCar);
 
       containerCards.appendChild(divCard);
     });
