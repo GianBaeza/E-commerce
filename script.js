88960eccd84f0a7c7f82772b9685f881ef21bf25
GianBaeza <gianb04@gmail.com>
2024-06-12 10:52:02 -0300

    home

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 const nav = document.querySelector("#nav");
+const carrusel = document.querySelector(".carrusel-items");
 
 document.addEventListener("DOMContentLoaded", () => {
   abrir.addEventListener("click", () => {
@@ -8,5 +9,30 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   cerrar.addEventListener("click", () => {
     nav.classList.remove("nav-visible");
+  });
+
+  let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+  let intervalo = null;
+  let step = 1;
+  const start = () => {
+    intervalo = setInterval(function () {
+      carrusel.scrollLeft = carrusel.scrollLeft + step;
+      if (carrusel.scrollLeft == maxScrollLeft) {
+        step = step * 1;
+      } else if (carrusel.scrollLeft <= 0) {
+        step = step * -1;
+      }
+    }, 10);
+  };
+
+  const stop = () => {
+    clearInterval(intervalo);
+  };
+
+  carrusel.addEventListener("mouseover", () => {
+    stop();
+  });
+  carrusel.addEventListener("mouseout", () => {
+    start();
   });
 });
